29e2c283a0477638fc713cc71229a88a420325c2
jasonhsu <122219963+rhino0321@users.noreply.github.com>
2023-08-08 19:22:09 +0800

    Add files via upload

---
+++ OOP/hw2.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+using namespace std;
+
+template<class T>
+class Node
+{
+    public:
+        Node(T val);
+        void main(Node<T>*);
+        T data;
+        ~Node(){
+            delete left;
+            delete right;
+        }
+    private:
+        Node* left;
+        Node* right;
+        Node<T>* insert(Node<T>*, T);
+        void printInOrder(const Node<T>*) const;
+};
+
+int main()
+{
+    string type;
+    getline(cin, type);
+    if (type == "char")
+    {
+        Node<char>* root = nullptr;
+        root->main(root);
+    }
+    else if (type == "int")
+    {
+        Node<int>* root = nullptr;
+        root->main(root);
+    }
+    return 0;
+}
+
+template<class T>
+Node<T>::Node(T val) : data(val), left(nullptr), right(nullptr){}
+
+template<class T>
+Node<T>* Node<T>::insert(Node* root, T num)
+{
+    if (root == nullptr)
+        return new Node<T>(num);
+    if (root->data > num)
+        root->left = insert(root->left, num);
+    else
+        root->right = insert(root->right, num);
+    return root;
+}
+
+template<class T>
+void Node<T>::printInOrder(const Node<T>* root) const
+{
+    if (root == nullptr)
+        return;
+    printInOrder(root->left);
+    cout << root->data << " ";
+    printInOrder(root->right);
+}
+
+template<class T>
+void Node<T>::main(Node<T>* root)
+{
+    T num;
+    string inputline;
+    getline(cin, inputline);
+    istringstream iss(inputline);
+    while (iss >> num)
+    {
+        root = root->insert(root, num);
+    }
+    root->printInOrder(root);
+    cout << endl;
+    delete root;
+}
